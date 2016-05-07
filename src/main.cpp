e59975ab84b7486bea4dfa3be6b6445645a7934b
Duality4y <201292@live.nl>
2016-05-07 15:13:46 +0200

    trying to add upload commandline switch

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include <avr/io.h>
 
+#define NUMROWS 8
+#define NUMCOLS 14
+#define NUMKEYS 49
+#define START_NOTE 36
+
+typedef struct
+{
+    uint8_t start_id;
+    uint8_t end_id;
+    uint64_t start_time;
+    uint64_t end_time;
+    bool triggerd;
+} note_t;
 
 int main(void)
 {
